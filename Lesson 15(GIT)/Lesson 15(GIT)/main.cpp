ff2e6f9c9d581a825f3bed104f159225491edc34
vadimka-8888 <73032993+vadimka-8888@users.noreply.github.com>
2021-10-26 16:35:06 +0300

    Add new function IsLeapYear

---
+++ Lesson 15(GIT)/Lesson 15(GIT)/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool IsLeapYear(int year)
+{
+	if (year % 4 == 0)
+	{
+		if (year % 100 != 0)
+		{
+			return true;
+		}
+		else if (year % 400 == 0)
+		{
+			return true;
+		}
+		else return false;
+	}
+	else return false;
+}
+
+
+int main()
+{
+
+}
